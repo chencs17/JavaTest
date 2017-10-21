c8a92325cadc140191cc6168061068d561e325dc
chencs17 <2227619802@qq.com>
2017-10-21 22:09:08 +0800

    增加了一个java文件，关于int的最大最小值

---
+++ ch02/TestDemoIntMaxMin.java
@@ -0,0 +1,11 @@
+public class TestDemoIntMaxMin{
+	public static void main(String args[]){
+		int max = Integer.MAX_VALUE;
+		int min = Integer.MIN_VALUE;
+		System.out.println(max);
+		System.out.println(min);
+		System.out.println(max + 1);
+		System.out.println(min - 1);
+		System.out.println(min - 2);
+	}
+}
