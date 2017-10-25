237b4d437699b93423d58546ad09beb35d52c44b
chencs17 <2227619802@qq.com>
2017-10-25 22:10:24 +0800

    类型转化程序

--- ch02/TestDemoType.java
+++ ch02/TestDemoType.java
@@ -5,5 +5,10 @@ public class TestDemoType{
 		System.out.println(max+1L);
 		System.out.println(min-(long)1);
 		System.out.println((long)min-2);
+		int num = 130;
+		byte x = (byte)num;//byte的范围是-128到127
+		System.out.println(x);
+		byte y = 100;
+		System.out.println(y);
 	}
 }
