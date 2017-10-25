e182035b975597046bfdf3d57cd09f4a2ca89fe6
chencs17 <2227619802@qq.com>
2017-10-25 21:41:05 +0800

    增加了两个新的文件，作用是两种类型转化

---
+++ ch02/TestDemoType.java
@@ -0,0 +1,9 @@
+public class TestDemoType{
+	public static void main(String args[]){
+		int max = Integer.MAX_VALUE;
+		int min = Integer.MIN_VALUE;
+		System.out.println(max+1L);
+		System.out.println(min-(long)1);
+		System.out.println(min-(long)2);
+	}
+}
