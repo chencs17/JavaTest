e0e2e38da25f584adfa8af51649f6fae58f4b601
chencs17 <2227619802@qq.com>
2017-11-04 15:30:44 +0800

    数组的使用

---
+++ ch03/Array1/TestDemo.java
@@ -0,0 +1,11 @@
+public class TestDemo{
+	public static void main(String args[]){
+		int data[] = new int[3];
+		data[0] = 10;
+		data[1] = 20;
+		data[2] = 30;
+		for(int d : data){
+			System.out.print(d + "、");
+		}
+	}
+}	
